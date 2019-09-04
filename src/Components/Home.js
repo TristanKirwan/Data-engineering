1a75912a22cc2631e9691a136f337d9acd99bb19
TristanKirwan <33831870+TristanKirwan@users.noreply.github.com>
2019-09-04 18:59:38 +0200

    Initial
    Geen idee meer wat hier allemaal in zit.

---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import BigCard from './Bigcard'
+
+export default function Home() {
+    return (
+            <div className="wrapper">
+                <p>Start building your favourite decks today!</p>
+                <BigCard/>
+            </div>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <ul className="hrflex navbar">
+                <li>Home</li>
+                <li>Cards</li>
+                <li>Decks</li>
+                <li>Articles</li>
+                <li>Register</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    )
+}

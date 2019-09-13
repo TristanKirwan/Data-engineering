ef270fbb2156baba3e295dd7a207a4f0fd3ba3ed
TristanKirwan <33831870+TristanKirwan@users.noreply.github.com>
2019-09-13 19:47:56 +0200

    Commit with bootstrap inits

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
+import './style.scss'
 
-export default function Navbar() {
-    return (
-        <div>
-            <ul className="hrflex navbar">
-                <li>Home</li>
-                <li>Cards</li>
-                <li>Decks</li>
-                <li>Articles</li>
-                <li>Register</li>
-                <li>Login</li>
-            </ul>
-        </div>
-    )
+type NavbarState = {
+    isOpen: boolean
+}
+export default class Navbar extends React.Component<{}, NavbarState> {
+    constructor(props: any){
+        super(props)
+        this.state={
+            isOpen: false
+        }
+    }
+
+    render(){
+        return (
+            <div  className="nav">
+                <ul className="nav-expanded">
+                    <li><a href="#" className="p">Home</a></li>
+                    <li><a href="#" className="p">Cards</a></li>
+                    <li><a href="#" className="p">Decks</a></li>
+                    <li><a href="#" className="p">Articles</a></li>
+                    <li><a href="#" className="p">Register</a></li>
+                    <li><a href="#" className="p">Login</a></li>
+                </ul>
+                <div className="nav-collapsed">
+                    <i className="fas fa-bars" onClick={() => this.setState({isOpen: !this.state.isOpen})}></i>
+                    {this.state.isOpen? 
+                        <div className="nav-collapsed__menu">
+                            <ul>
+                                <li><a href="#" className="p">Home</a></li>
+                                <li><a href="#" className="p">Cards</a></li>
+                                <li><a href="#" className="p">Decks</a></li>
+                                <li><a href="#" className="p">Articles</a></li>
+                                <li><a href="#" className="p">Register</a></li>
+                                <li><a href="#" className="p">Login</a></li>
+                            </ul>
+                        </div> : null}
+                </div>
+            </div>
+        )}
 }

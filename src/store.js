646d9c94801e1820091a8e4374d95feda26ddc7d
Tristan Kirwan <0944035@hr.nl>
2020-06-15 13:23:36 +0200

    Update store.js
    Commented out redux devtools.

--- src/store.js
+++ src/store.js
@@ -13,7 +13,7 @@ const store = createStore(
     //This allows the use of redux dev tools in chrome
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 )
 
